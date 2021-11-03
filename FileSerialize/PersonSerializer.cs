506015117c035c6530214602cdb9e294cc91576a
pethaf <pethaf1@gmail.com>
2021-11-03 09:39:21 +0100

    Add project files.

---
+++ FileSerialize/PersonSerializer.cs
@@ -0,0 +1,39 @@
+﻿using FilesSerialize;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSerialize
+{
+    class PersonSerializer
+    {
+        private string _path;
+
+        public PersonSerializer(string thePath)
+        {
+            this._path = thePath;
+        }
+        public void SerializeInOneFile(List<FilesSerialize.Person> Lop)
+        {
+            string completePath = System.IO.Path.Join(_path, "persons.txt");
+            using(StreamWriter sw = new StreamWriter(completePath))
+            {
+               
+                foreach (var person in Lop)
+                {
+                    sw.WriteLine($"{person.Id},{person.Name},{person.Age}");
+                }
+            }
+        }
+        public void SerializeIManyFiles(List<Person> LOP)
+        {
+            foreach(Person thePerson in LOP)
+            {
+
+            }
+        }
+    }
+}
---
+++ FileSerialize/Program.cs
@@ -0,0 +1,16 @@
+﻿using FileSerialize;
+using System;
+using System.IO;
+
+namespace FilesSerialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonSerializer ps = new($"{Path.Join(Directory.GetCurrentDirectory(), "slask")}");
+            ps.SerializeInOneFile(Data.People);
+
+        }
+    }
+}
